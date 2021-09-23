1816aea50a79e7b511038590854d1954c2b5e496
Владислав Овчинников <vovchinniko@telematika.com>
2021-09-24 01:51:42 +0300

    initial commit

--- deploy/helm-task/migrations/V2_0_0__test.sql
+++ deploy/helm-task/migrations/V2_0_0__test.sql
@@ -1,5 +1,41 @@
-create table azula
+create type permissions as enum ('USER_UPDATE', 'USER_READ', 'USER_DELETE');
+
+alter type permissions owner to postgres;
+
+create table role
 (
-    id    int,
-    name varchar
+    name        varchar not null
+        constraint role_pk
+            primary key,
+    permissions character varying[]
 );
+
+alter table role
+    owner to postgres;
+
+create table users
+(
+    id       bigint default nextval('user_id_seq'::regclass) not null
+        constraint user_pk
+            primary key,
+    login    varchar                                         not null,
+    password varchar                                         not null,
+    name     varchar                                         not null,
+    role     varchar                                         not null
+        constraint users_role_name_fk
+            references role
+            on update cascade
+);
+
+alter table users
+    owner to postgres;
+
+create unique index user_id_uindex
+    on users (id);
+
+create unique index users_login_uindex
+    on users (login);
+
+create unique index role_name_uindex
+    on role (name);
+
